bbd94ee798c4ecf6bcd63c1dbc0ae4b4f835d7fa
rfallarialhc <c.fallaria@lionheart.asia>
2025-05-06 11:42:16 +0800

    added validation

--- src/Helpers/Validate.php
+++ src/Helpers/Validate.php
@@ -21,4 +21,49 @@ class Validate
 
         return v::email()->validate($email) === true;
     }
+
+    public static function isValidNumber(string | null $number): bool
+    {
+        if (empty($number)) {
+            return true;
+        }
+
+        return v::number()->validate($number) === true;
+    }
+
+    public static function isValidLength(string | null $text, int | null $min = null, int | null $max = null): bool
+    {
+        if (empty($text)) {
+            return true;
+        }
+
+        return v::length($min, $max)->validate($text) === true;
+    }
+
+    public static function isValidPhone(string | null $phone): bool
+    {
+        if (empty($phone)) {
+            return true;
+        }
+
+        return v::phone()->validate($phone) === true;
+    }
+
+    public static function isValidCode(string | null $code): bool
+    {
+        if (empty($code)) {
+            return true;
+        }
+
+        return v::numericVal()->validate($code) === true;
+    }
+
+    public static function isValidSize(string | null $file, string | null $size): bool
+    {
+        if (empty($file)) {
+            return true;
+        }
+
+        return v::size($size)->validate($file) === true;
+    }
 }
--- src/Models/Form.php
+++ src/Models/Form.php
@@ -5,6 +5,7 @@ namespace TofuPlugin\Models;
 use TofuPlugin\Consts;
 use TofuPlugin\Helpers\Form as FormHelper;
 use TofuPlugin\Structure\FormConfig;
+use TofuPlugin\Helpers\Validate;
 
 class Form
 {
@@ -151,12 +152,82 @@ class Form
             wp_die('Nonce verification failed.', 'TOFU Nonce Error', ['response' => 403]);
         }
 
-        /** @todo Validate input field */
+        // Validate input field
         $validated = $_POST;
+        $errorFlag = false;
+        $error = [];
+
+        foreach ($this->config->validation->rules as $key => $rule) {
+            $value = $_POST[$key] ?? null;
+
+            // Check for required fields
+            if (!empty($rule['required']) && empty($value)) {
+                $errorFlag = true;
+                $error[$key] = sprintf('Field "%s" is required.', $key);
+                continue;
+            }
+
+            // Check for valid email
+            if (!empty($rule['email']) && !Validate::isValidEmail($value)) {
+                $errorFlag = true;
+                $error[$key] = sprintf('Field "%s" must be a valid email address.', $key);
+                continue;
+            }
+
+            // Check if number
+            if (!empty($rule['number']) && !Validate::isValidNumber($value)) {
+                $errorFlag = true;
+                $error[$key] = sprintf('Field "%s" must be a number.', $key);
+                continue;
+            }
+
+            // Check min
+            if (!empty($rule['min']) && !Validate::isValidLength($value, $rule['min'])) {
+                $errorFlag = true;
+                $error[$key] = sprintf('Field "%s" length must be greater than or equals to %s.', $key, $rule['min']);
+                continue;
+            }
+
+            // Check max
+            if (!empty($rule['max']) && !Validate::isValidLength($value, null, $rule['max'])) {
+                $errorFlag = true;
+                $error[$key] = sprintf('Field "%s" length must be less than or equals to %s.', $key, $rule['max']);
+                continue;
+            }
+
+            // Check if valid phone number
+            if (!empty($rule['phone']) && !Validate::isValidPhone($value)) {
+                $errorFlag = true;
+                $error[$key] = sprintf('Field "%s" must be a valid phone number.', $key);
+                continue;
+            }
+
+            // Check if valid code
+            if (!empty($rule['code']) && !Validate::isValidCode($value)) {
+                $errorFlag = true;
+                $error[$key] = sprintf('Field "%s" must be a valid code.', $key);
+                continue;
+            }
+
+            // Check if valid size
+            if (!empty($rule['file_size']) && !Validate::isValidCode($value, $rule['file_size'])) {
+                $errorFlag = true;
+                $error[$key] = sprintf('File "%s" must be at least "%s".', $key, $rule['file_size']);
+                continue;
+            }
+        }
 
         // Store the input values in the transient
         $this->values = $validated;
         $this->storeValues();
+
+        // Store errors if any
+        if($errorFlag) {
+            $redirect = $this->config->template->inputPath;
+            FormHelper::setError($error);
+            wp_redirect($redirect);
+            exit;
+        }
 
         // Redirect to the confirmation page
         $url = $this->config->template->confirmPath;
